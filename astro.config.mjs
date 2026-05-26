215c793573a87ff003ade9a837f8190e31f8eb97
legongoraek <104285100+legongoraek@users.noreply.github.com>
2026-05-26 14:42:23 -0600

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 export default defineConfig({
   integrations: [
     react(),
+    icon({
+      defaultStyle: 'width: 1.5em; height: 1.5em;'
+    })
   ],
   site: 'https://luisenriquegongoraek.com',
   output: 'static',
